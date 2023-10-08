08cff3458296897a06de02b0b3b56a222c19000a
Daalmodi <daalmodi@gmail.com>
2023-10-08 16:47:57 -0500

    carrusel version 1

---
+++ js/carrusel.js
@@ -0,0 +1,20 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
